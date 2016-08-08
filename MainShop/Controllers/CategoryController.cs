8822b6a9347c9c0b0d6ddcf3eb9369a79c64016a
chinhnb <bachinh2105@gmail.com>
2016-08-08 17:11:12 +0700

    62

---
+++ MainShop/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category/Created        
+        public ActionResult Created()
+        {
+            return View();
+        }
+
+        // GET: Category/List
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
