700566b0726129105a848678aac62359ed5adb6f
chinhnb <bachinh2105@gmail.com>
2016-08-09 23:48:51 +0700

    ok

--- MainShop/Controllers/CategoryController.cs
+++ MainShop/Controllers/CategoryController.cs
@@ -8,16 +8,10 @@ namespace MainShop.Controllers
 {
     public class CategoryController : Controller
     {
-        // GET: Category/Created        
-        public ActionResult Created()
+        // GET: Category        
+        public ActionResult Index()
         {
             return View();
-        }
-
-        // GET: Category/List
-        public ActionResult List()
-        {
-            return View();
-        }
+        }        
     }
 }
